ae61a2761ff6ac72fd1827be8237d6b59f7bc715
nerusskyhigh <Guglielmo.grillo@gmail.com>
2021-09-25 18:27:09 +0200

    add check for the working directory in order to not delete important files

--- clean_project.sh
+++ clean_project.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+if [ "$PWD" != "/home/guglielmo/Desktop/GithubRepositories/labIV" ]; then
+  echo "I think I'm in the wrong directory."
+  echo "I'll stop in ordert to not delete important files"
+  exit;
+fi
+
 find . -type f  ! -name '*.v' \
                 ! -name '*.bit' \
                 ! -name '*.xise' \
