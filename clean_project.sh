60b2f01f8403f15a3832813b55ddc804868eacc5
nerusskyhigh <Guglielmo.grillo@gmail.com>
2021-11-03 19:55:58 +0100

    update .gitignore and clean_project.sh with the new file extension .ngc

--- clean_project.sh
+++ clean_project.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
-if [ "$PWD" != "/home/guglielmo/Desktop/GithubRepositories/labIV" ]; then
+if [ "$PWD" != "/home/guglielmo/GithubRepositories/labIV" ]; then
   echo "I think I'm in the wrong directory."
   echo "I'll stop in ordert to not delete important files"
   exit;
 fi
 
-find . -type f  ! -name '*.v' \
+find . -type f  ! -path './.git/*' \
+                ! -name '*.gitignore' \
+                      \
+                ! -name 'clean_project.sh' \
+                ! -name '*.pdf' \
+                ! -name '*.so.*' \
+                      \
+                ! -name '*.v' \
                 ! -name '*.bit' \
                 ! -name '*.xise' \
-                ! -path './.git/*' \
-                ! -name '*.so.*' \
                 ! -name '*.ucf' \
-                ! -name '*.pdf' \
-                ! -name '*.gitignore' \
-                ! -name 'clean_project.sh' \
                 ! -name '*.ngc' \
           -delete
 
